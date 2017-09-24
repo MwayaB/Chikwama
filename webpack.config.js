141ada6d5b456f0beccd595b3e2e192fb2b3bddc
Mwayanjana Bolokonya <mwayab@gmail.com>
2017-09-24 17:49:44 +0200

    First Chikwama commit

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var webpack = require("webpack");
+var path = require("path");
+const www = path.resolve(__dirname, "www");
+
+module.exports = {
+  entry: "./src/app.js",
+  output: {
+    filename: "bundle.js",
+    path: www
+  },
+  module: {
+    rules: [],
+    loaders: [
+      {
+        test: /\.json$/,
+        use: "json-loader"
+      }
+    ]
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery"
+    })
+  ],
+  devServer: {
+    contentBase: www
+  }
+}
