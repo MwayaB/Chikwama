1f027ebe73002db98abaa7c21e33fb82736e7bfd
Mwayanjana Bolokonya <mwayab@gmail.com>
2017-10-25 18:42:55 +0200

    Updated chikwama contract

--- contracts/MakwachaToken.sol
+++ contracts/MakwachaToken.sol
@@ -6,57 +6,58 @@ contract Chikwama{
      {
         address add;
         uint value;
-        uint256 id;
+        bytes32 id;
         uint8 chikwamaType; //0 central office 1 Agent 2 EndUser
-        uint256 pin;
+        bytes32 pin;
     }
     
     uint256 public accountCount;
-    mapping(uint256 => address) accounts;
-    mapping(address => mapping(uint256 => Account)) chikwama;
+    mapping(bytes32 => address) accounts;
+    mapping(address => mapping(bytes32 => Account)) chikwama;
     
-    event CreateAccount(uint256 NationalID, address ChikwamaAddress);
+    event CreateAccount(bytes32 NationalID, address ChikwamaAddress);
     
     function createChikwama(address _add, uint256 _value, uint256 _id, uint8 _chikwamaType, uint256 _pin) public returns(bool)
     {
-        if (accounts[_id]==0x0)
+        if (accounts[keccak256(_id)]==0x0)
         {
-            accounts[_id] = _add;
-            chikwama[_add][_id].add = _add;
-            chikwama[_add][_id].id = _id;
-            chikwama[_add][_id].value = _value;
-            chikwama[_add][_id].chikwamaType = _chikwamaType;
-            chikwama[_add][_id].pin = _pin;
+            accounts[keccak256(_id)] = _add;
+            chikwama[_add][keccak256(_id)].add = _add;
+            chikwama[_add][keccak256(_id)].id = keccak256(_id);
+            chikwama[_add][keccak256(_id)].value = _value;
+            chikwama[_add][keccak256(_id)].chikwamaType = _chikwamaType;
+            chikwama[_add][keccak256(_id)].pin = keccak256(_pin);
             accountCount++;
-            CreateAccount(_id,_add);
+            CreateAccount(keccak256(_id),_add);
             return true;
         }
 
     }
     
     function getChikwama(uint256 _id) public constant returns(address) {
-        address _add = accounts[_id];
+        address _add = accounts[keccak256(_id)];
       
-        return chikwama[_add][_id].add;
+        return chikwama[_add][keccak256(_id)].add;
     
     }
     
     function checkPin(uint256 _id, uint256 _pin) public constant returns(bool){
-        address _add = accounts[_id];
-        if(chikwama[_add][_id].pin==_pin)return true;
+        address _add = accounts[keccak256(_id)];
+        if(chikwama[_add][keccak256(_id)].pin==keccak256(_pin))return true;
     }
     
     function changePin(uint256 _id, uint256 _pin, uint256 _newPin) public returns(bool)
     {
-        address _add = accounts[_id];
-        if(chikwama[_add][_id].pin == _pin)
+        address _add = accounts[keccak256(_id)];
+        if(chikwama[_add][keccak256(_id)].pin == keccak256(_pin))
         {
-            chikwama[_add][_id].pin = _newPin;
+            chikwama[_add][keccak256(_id)].pin = keccak256(_newPin);
             return true;
         }
         else return false;
     }
 }
+
 
 
 contract MakwachaToken {
